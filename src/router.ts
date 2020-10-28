1b8d2fe207cc50ddf9b28ab275c91305f749c896
ProPanesar <panesar.pb08@gmail.com>
2020-10-28 07:57:35 -0400

    update

--- src/router.ts
+++ src/router.ts
@@ -1,24 +1,25 @@
 import express, { Request, Response, NextFunction } from 'express';
-import { findById, findAll, insertUser, deleteUser, updateUser, User } from './users';
+import * as users from './users';
+import { User } from './users';
 
 const router = express.Router();
 
 router.get('/', (request: Request, response: Response, next: NextFunction) => {
 	response.status(200).json({
-		users: findAll()
+		users: users.findAll()
 	});
 });
 
 router.get('/:id', (request: Request, response: Response, next: NextFunction) => {
 	const id: number = Number(request.params.id);
 	response.status(200).json({
-		user: findById(id)
+		user: users.findById(id)
 	});
 });
 
 router.post('/', (request: Request, response: Response, next: NextFunction) => {
 	const userBody: User = request.body;
-	insertUser(userBody);
+	users.insertUser(userBody);
 	response.status(200).json({
 		message: 'success'
 	});
@@ -26,7 +27,7 @@ router.post('/', (request: Request, response: Response, next: NextFunction) => {
 
 router.put('/', (request: Request, response: Response, next: NextFunction) => {
 	const userBody: User = request.body;
-	insertUser(userBody);
+	users.updateUser(userBody);
 	response.status(200).json({
 		message: 'success'
 	});
@@ -35,7 +36,7 @@ router.put('/', (request: Request, response: Response, next: NextFunction) => {
 
 router.delete('/:id', (request: Request, response: Response, next: NextFunction) => {
 	const id: number = Number(request.params.id);
-	deleteUser(id);
+	users.deleteUser(id);
 	response.status(200).json({
 		message: 'success'
 	});
