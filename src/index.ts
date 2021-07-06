26a6e78d596f7d0c9cf29409f53cf6b858fd9468
ProPanesar <panesar.pb08@gmail.com>
2021-07-06 03:55:23 -0400

    refactor

--- src/index.ts
+++ src/index.ts
@@ -15,4 +15,6 @@ app.use(router);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server running on port: ${port}`));
+app.listen(port, () => { 
+	console.log(`Server running on port: ${port}`)
+});
